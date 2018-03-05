2c1fa94f6c6b71fe2b87e6b24d73a0f76134be60
Robert Niederreiter <office@squarewave.at>
2018-03-05 10:36:31 +0100

    WIP

--- src/bda/plone/gtm/viewlets.py
+++ src/bda/plone/gtm/viewlets.py
@@ -5,25 +5,27 @@ from plone.app.layout.viewlets.common import ViewletBase
 
 
 GTM_SCRIPT = """
-<script>{layer_name} = [];</script>
+<script>%(layer_name)s = [];</script>
 <!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','{layer_name}','{container_id}');</script>
+})(window,document,'script','%(layer_name)s','%(container_id)s');</script>
 <!-- End Google Tag Manager -->
 """
 
 GTM_NO_SCRIPT = """
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id={container_id}"
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=%(container_id)s"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
 """
 
 
 class GTMSettings(object):
+    """Google Tag Manager settings mixin.
+    """
 
     @property
     def settings(self):
@@ -36,17 +38,15 @@ class GTMLoaderViewlet(ViewletBase, GTMSettings):
     """
 
     def render(self):
-        print 80 * '#'
-        print 'GTMLoaderViewlet.render()'
         settings = self.settings
-        return GTM_SCRIPT.format(
+        return GTM_SCRIPT % dict(
             layer_name=settings.layer_name,
             container_id=settings.container_id
         )
 
 
 class GTMDataViewlet(ViewletBase, GTMSettings):
-    """Context specific Google Tag Manager viewlet.
+    """Google Tag Manager data viewlet.
     """
 
     @property
@@ -58,14 +58,12 @@ class GTMDataViewlet(ViewletBase, GTMSettings):
     def render(self):
         """Render script tag pushing context related data to GTM layer.
         """
-        print 80 * '#'
-        print 'GTMDataViewlet.render()'
         settings = self.settings
         tags = list()
         for k, v in self.data:
-            tags.append("'{k}':'{v}'".format(k=k, v=v))
-        return u'{no_script}<script>{layer_name}.push({{{data}}})</script>'.format(
-            no_script=GTM_NO_SCRIPT.format(container_id=settings.container_id),
+            tags.append("'%(k)s':'%(v)s'" % dict(k=k, v=v))
+        return u'%(no_script)s<script>%(layer_name)s.push({%(data)s})</script>' % dict(
+            no_script=GTM_NO_SCRIPT % dict(container_id=settings.container_id),
             layer_name=self.settings.layer_name,
             data=u','.join(tags)
         )
