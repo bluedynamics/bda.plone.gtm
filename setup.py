72bb488a922f6f2acbea3d8269d1d8fe10f85537
Peter Holzer <peter.holzer@agitator.com>
2018-06-06 18:19:15 +0200

    Back to development: 1.0a2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-version = '1.0a1'
+version = '1.0a2.dev0'
 shortdesc = "Google Tag Manager Intergation"
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'CHANGES.rst')).read()
